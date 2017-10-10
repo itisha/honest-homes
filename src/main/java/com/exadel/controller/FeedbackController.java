ff4f95f3bb118f7131013c9f2dfb5400f5784f3d
Tsikhan Kuprevich <tkuprevich@exadel.com>
2017-10-10 18:13:32 +0300

    Service layer

--- src/main/java/com/exadel/controller/FeedbackController.java
+++ src/main/java/com/exadel/controller/FeedbackController.java
@@ -1,7 +1,7 @@
 package com.exadel.controller;
 
 import com.exadel.mongodb.model.Feedback;
-import com.exadel.mongodb.repository.FeedbackRepository;
+import com.exadel.mongodb.service.api.FeedbackService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,48 +18,48 @@ import java.util.List;
 public class FeedbackController {
 
 
-    private final FeedbackRepository repository;
+    private final FeedbackService feedbackService;
 
     @Autowired
-    public FeedbackController(FeedbackRepository repository) {
-        this.repository = repository;
+    public FeedbackController(FeedbackService feedbackService) {
+        this.feedbackService = feedbackService;
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity getAll() {
-        return new ResponseEntity<List<Feedback>>(repository.findAll(), HttpStatus.OK);
+        return new ResponseEntity<List<Feedback>>(feedbackService.findAll(), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/id/{id}", method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity findById(@PathVariable("id") String id) {
-        return new ResponseEntity<Feedback>(repository.findOne(id), HttpStatus.OK);
+        return new ResponseEntity<Feedback>(feedbackService.findOne(id), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/authorId/{authorId}", method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity findByAuthorId(@PathVariable("authorId") String landlordId) {
-        return new ResponseEntity<List<Feedback>>(repository.findByAuthorId(landlordId), HttpStatus.OK);
+        return new ResponseEntity<List<Feedback>>(feedbackService.findByAuthorId(landlordId), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/entityId/{entityId}", method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity findByEntityId(@PathVariable("authorId") String landlordId) {
-        return new ResponseEntity<List<Feedback>>(repository.findByEntityId(landlordId), HttpStatus.OK);
+        return new ResponseEntity<List<Feedback>>(feedbackService.findByEntityId(landlordId), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST, produces = "application/json")
     public ResponseEntity create(@RequestBody Feedback property) {
-        return new ResponseEntity<Feedback>(repository.save(property), HttpStatus.CREATED);
+        return new ResponseEntity<Feedback>(feedbackService.save(property), HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "/", method = RequestMethod.PUT, produces = "application/json")
     public ResponseEntity update(@RequestBody Feedback property) {
-        return new ResponseEntity<Feedback>(repository.save(property), HttpStatus.CREATED);
+        return new ResponseEntity<Feedback>(feedbackService.save(property), HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = "application/json")
     public ResponseEntity delete(@PathVariable("id") String id) {
         HttpStatus status = HttpStatus.NOT_FOUND;
-        if (repository.exists(id)) {
-            repository.delete(id);
+        if (feedbackService.exists(id)) {
+            feedbackService.delete(id);
             status = HttpStatus.ACCEPTED;
         }
         return new ResponseEntity(status);
